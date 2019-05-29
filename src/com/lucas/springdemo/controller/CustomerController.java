69f563d792dbd00eda13e15e3d9de9ab168fe252
Lucas Couto <loc.unb@gmail.com>
2019-05-29 17:21:54 -0300

    refactoring: added a service layer for customer

--- src/com/lucas/springdemo/controller/CustomerController.java
+++ src/com/lucas/springdemo/controller/CustomerController.java
@@ -5,24 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.lucas.springdemo.dao.CustomerDAO;
 import com.lucas.springdemo.entity.Customer;
+import com.lucas.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("customer")
 public class CustomerController {
 	
-	// need to inject the customerDAO
+	// need to inject the customerService
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("list")
+	@GetMapping("list")
 	public String listCustomers(Model model) {
 		
-		// get customers from DAO
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		// get customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		// add the customers to the model
 		model.addAttribute("customers", theCustomers);
---
+++ src/com/lucas/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.lucas.springdemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lucas.springdemo.dao.CustomerDAO;
+import com.lucas.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+	
+	// need to inject customerDAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+}
